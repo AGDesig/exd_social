df896c40257321228fef833340dca3243ab2382e
Ahmad ghauri <Ahmadghauri.078@gmail.com>
2023-01-27 19:09:03 +0500

    recently changed things on it

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -130,7 +130,7 @@ class LoginScreen extends StatelessWidget {
                               onPressed: () {
                                 Get.to(SignupScreen());
                               },
-                              child: Text("Not a User! tap to Signup here")),
+                              child: Text("Not a user")),
                         )
                       ],
                     )),
